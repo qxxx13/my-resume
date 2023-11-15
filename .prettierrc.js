9616404a74910c96d32edd285a69309d102aa9c6
Nikolay Nazarenko <redecv9@gmail.com>
2023-11-16 00:16:14 +0300

    deploy workflow

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 4,
-  endOfLine: 'auto',
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    endOfLine: 'auto',
 };
 
 // {
